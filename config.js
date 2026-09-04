1d187621ee9b0872e3331798a5c3731837931482
ankitapkdeveloper <ankitapkdeveloper@gmail.com>
2026-09-04 21:57:54 +0530

    Refactor Supabase configuration object structure

--- config.js
+++ config.js
@@ -1,9 +1,4 @@
-// Supabase setup
-// 1. Create a Supabase project.
-// 2. Put your Project URL and Publishable key below.
-// 3. Never put a Supabase service_role/secret key here.
-
 window.SNAKE_CONFIG = {
-  const SUPABASE_URL = "https://valezabojkidhfmmmnrx.supabase.co";
-const SUPABASE_KEY = "sb_publishable_dU6D8DaQ6dkL8rbDIp-VGw_0ePcZq-G";
+    SUPABASE_URL: "https://valezabojkidhfmmmnrx.supabase.co",
+    SUPABASE_KEY: "sb_publishable_dU6D8DaQ6dkL8rbDIp-VGw_0ePcZq-G"
 };
