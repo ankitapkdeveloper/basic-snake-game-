3725bac83883713020c82999be7ec822c4d0b74f
ankitapkdeveloper <ankitapkdeveloper@gmail.com>
2026-09-04 22:05:30 +0530

    Fix config object property names for Supabase

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SNAKE_CONFIG = {
-    SUPABASE_URL: "https://valezabojkidhfmmmnrx.supabase.co",
-    SUPABASE_KEY: "sb_publishable_dU6D8DaQ6dkL8rbDIp-VGw_0ePcZq-G"
+  supabaseUrl: "https://valezabojkidhfmmmnrx.supabase.co",
+  supabaseKey: "sb_publishable_dU6D8DaQ6dkL8rbDIp-VGw_0ePcZq-G"
 };
